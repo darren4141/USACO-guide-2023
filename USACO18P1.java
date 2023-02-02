0abaec791708bc405bf78cd4b3357999a82c3b6a
darren4141 <109255883+darren4141@users.noreply.github.com>
2023-02-02 10:10:48 -0500

    usaco18p1

---
+++ USACO18P1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class USACO18P1{
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("teleport.in"));
+        PrintWriter pw = new PrintWriter("teleport.out");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int dist = Math.abs(b-a);
+
+        if(Math.abs(a-x) + Math.abs(b-y) < dist){
+            dist = Math.abs(a-x) + Math.abs(b-y);
+        }
+
+        if(Math.abs(a-y) + Math.abs(b-x) < dist){
+            dist = Math.abs(a-y) + Math.abs(b-x);
+        }
+
+        pw.println(dist);
+        pw.close();
+    }
+
+}
